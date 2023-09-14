bc4409ddf5ffa92db5e10d9c87722d39f0224290
imane sbiti <emysb2020@gmail.com>
2023-09-13 23:21:27 -0400

    finalisation formulaire message erreur

--- js/ValidationForm.js
+++ js/ValidationForm.js
@@ -5,20 +5,24 @@ const validateForm = () => {
     const ville = document.getElementById('ville');
     const province = document.getElementById('province');
     const codepostal = document.getElementById('codepostal');
+    const email = document.getElementById('email');
+    const telephone = document.getElementById('telephone');
+    const message = document.getElementById('message');
 
-    console.log(prenom);
-    console.log(nom);
     const prenomValue = prenom.value.trim();
     const nomValue = nom.value.trim();
     const villeValue = ville.value.trim();
     const provinceValue = province.value.trim();
     const codepostalValue = codepostal.value.trim();
+    const emailValue = email.value.trim();
+    const telephoneValue = telephone.value.trim();
+    const messageValue = message.value.trim();
 
     let noError = true;
 
-    //Validation du prénom
+    //Validation du prenom
     if(prenomValue === '') {
-        setError(prenom, 'Le prénom ne peut pas être vide');
+        setError(prenom, 'Prénom vide !');
         noError = false;
     } else {
         setSuccess(prenom);
@@ -26,7 +30,7 @@ const validateForm = () => {
 
     //Validation du nom
     if(nomValue === '') {
-        setError(nom, 'Le nom ne peut pas être vide');
+        setError(nom, 'Nom vide !');
         noError = false;
     } else {
         setSuccess(nom);
@@ -34,75 +38,92 @@ const validateForm = () => {
 
     //Validation de la ville
     if(villeValue === '') {
-        setError(ville, 'La ville ne peut pas être vide');
+        setError(ville, 'Ville vide !');
         noError = false;
     } else {
         setSuccess(ville);
     }
 
-
     //Validation de la province
     if(provinceValue === '') {
-        setError(province, 'La province ne peut pas être vide');
+        setError(province, 'Province vide !');
         noError = false;
     } else {
         setSuccess(province);
     }
 
-
-    //Validation du nom
-    if(nomValue === '') {
-        setError(nom, 'Le nom ne peut pas être vide');
+    //Validation du code postal
+    if(codepostalValue === '') {
+        setError(codepostal, 'Code postal vide !');
         noError = false;
-    } else {
-        setSuccess(nom);
+    }
+    else if(!validateCodepostal(codepostalValue)){
+        setError(codepostal,'Code invalide, exp : G4R 9K9');
+        noError = false;
+    }
+    else {
+        setSuccess(codepostal);
     }
 
     //Validation du email
-    if(villeValue === ''){
-        setError(email,'Email ne peut être vide');
+    if(emailValue === ''){
+        setError(email,'Email vide !');
         noError = false;
     }
     else if(!validateEmail(emailValue)){
-        setError(email,'Entrez une adresse courriel valide');
+        setError(email,'Email non valide !');
         noError = false;
     }
     else{
         setSuccess(email);
     }
 
-    if(passwordValue === ''){
-        setError(password,'Mot de passe ne peut être vide');
-        noError = false;
-    }
-    else if(passwordValue.length < 8){
-        setError(password,'Mot de passe ne peut être plus petit que 8 caractères'); 
-        noError = false;
-    }
-    else{
-        setSuccess(password);
-    }
-
-    if(password2Value === '') {
-        setError(password2, 'Confirmez votre mot de passe');
+    //Validation du telephone
+    if(telephoneValue === '') {
+        setError(telephone, 'Téléphone vide !');
         noError = false;
     } 
-    else if (password2Value !== passwordValue) {
-        setError(password2, "Les mots de passe ne sont pas conformes");
+    else if(!validatetelephone(telephoneValue)){
+        setError(telephone,'Téléphone invalide : 514 333 4040 ');
         noError = false;
-    } 
+    }
     else {
-        setSuccess(password2);
+        setSuccess(telephone);
+    }
+    /*regex Pour
+    123-456-7890
+    (123) 456-7890
+    123 456 7890
+    123.456.7890
+    +91 (123) 456-7890
+    */
+
+    //Validation du message
+    if(messageValue === '') {
+        setError(message, 'Message vide !');
+        noError = false;
+    } else {
+        setSuccess(message);
     }
 
     console.log(noError);
     return noError;
 }
 
-const validateEmail = (email) => {
+function validateCodepostal(codepostal) {
+    /*const rep = /^[ABCEGHJ-NPRSTVXY][0-9][ABCEGHJ-NPRSTV-Z][ ]?[0-9][ABCEGHJ-NPRSTV-Z][0-9]$/;*/
+    const rep = /^([a-zA-Z]\d[a-zA-Z])\ {0,1}(\d[a-zA-Z]\d)$/;
+    return rep.test(String(codepostal).toLowerCase());
+}
+
+function validateEmail(email) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
     return re.test(String(email).toLowerCase());
+}
+
+function validatetelephone(telephone) {
+    const reg = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/;
+    return reg.test(String(telephone).toLowerCase());
 }
 
 const setError = (element,message) => { 
